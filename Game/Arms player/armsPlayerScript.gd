39f30c507c9fb786bd87c6bea5610f21798b4d8c
Paul <paul.de.miscault@gmail.com>
2024-10-01 14:10:33 +0200

    Implémentation ArmsPlayer

--- Game/Arms player/armsPlayerScript.gd
+++ Game/Arms player/armsPlayerScript.gd
@@ -1,8 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -540.0
+const SPEED = 150.0
 
 
 func _physics_process(delta: float) -> void:
@@ -10,12 +9,10 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("legsJump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# A FAIRE : suspension au plafond
 
 	# Get the input direction and handle the movement/deceleration.
-	var direction := Input.get_axis("legsLeft", "legsRight")
+	var direction := Input.get_axis("armsLeft", "armsRight")
 	if direction:
 		velocity.x = direction * SPEED
 		$Appearance.animation = "walk"
--- Game/Main/mainScript.gd
+++ Game/Main/mainScript.gd
@@ -1,11 +1,14 @@
 extends Node
 
 var legsPlayerSpawnPoint
+var armsPlayerSpawnPoint
 
 
 func _ready() -> void:
 	legsPlayerSpawnPoint = $TestLevel/LegsPlayerSpawnPoint.position
+	armsPlayerSpawnPoint = $TestLevel/ArmsPlayerSpawnPoint.position
 	$LegsPlayer.position = legsPlayerSpawnPoint
+	$ArmsPlayer.position = armsPlayerSpawnPoint
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
