ae024e94d48eaa07807df237e96c80d37636ef5a
Paul <paul.de.miscault@gmail.com>
2024-10-01 16:11:48 +0200

    Détection de proximité

--- Game/Main/mainScript.gd
+++ Game/Main/mainScript.gd
@@ -1,19 +1,33 @@
 extends Node
 
-var legsPlayerSpawnPoint
-var armsPlayerSpawnPoint
-
 
 func _ready() -> void:
-	legsPlayerSpawnPoint = $TestLevel/LegsPlayerSpawnPoint.position
-	armsPlayerSpawnPoint = $TestLevel/ArmsPlayerSpawnPoint.position
-	$LegsPlayer.position = legsPlayerSpawnPoint
-	$ArmsPlayer.position = armsPlayerSpawnPoint
+	SetSpawnPositions()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	UpdateCameraPosition()
+	
+	if (PlayerProximityDetected()):
+		$ProximityLabel.show()
+	else:
+		$ProximityLabel.hide()
+
+
+func SetSpawnPositions():
+	var legsPlayerSpawnPoint = $TestLevel/LegsPlayerSpawnPoint.position
+	var armsPlayerSpawnPoint = $TestLevel/ArmsPlayerSpawnPoint.position
+	$LegsPlayer.position = legsPlayerSpawnPoint
+	$ArmsPlayer.position = armsPlayerSpawnPoint
 
 
 func UpdateCameraPosition():
 	$Camera.position = ($ArmsPlayer.global_position + $LegsPlayer.global_position) / 2
+
+
+func PlayerProximityDetected() -> bool:
+	var distanceBetweenPlayers = $ArmsPlayer.global_position.distance_to($LegsPlayer.global_position)
+	if distanceBetweenPlayers < 500:
+		return true
+	else:
+		return false
