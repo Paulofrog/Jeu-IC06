33112cee9c038df9930b887dd2e19debed42c2cc
Romane-kulesza <74608881+Romane-kulesza@users.noreply.github.com>
2024-11-13 14:17:33 +0100

    startMenu buttons working

--- Game/UI/TitleScreen/title_screen_scene.gd
+++ Game/UI/TitleScreen/title_screen_scene.gd
@@ -5,6 +5,8 @@ var mainScene
 
 func _ready() -> void:
 	mainScene = preload("res://Game/Main/mainScene.tscn")
+	$Arms.play("default")
+	$Legs.play("default")
 
 
 func _on_start_button_pressed() -> void:
@@ -12,4 +14,12 @@ func _on_start_button_pressed() -> void:
 
 
 func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_packed(mainScene)
+
+
+func _on_stop_pressed() -> void:
 	get_tree().quit()
