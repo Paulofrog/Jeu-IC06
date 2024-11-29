4a5e81ab997549f2dd144c9f5020fbb10b5c3a31
Romane-kulesza <74608881+Romane-kulesza@users.noreply.github.com>
2024-11-29 18:29:45 +0100

    commit :)

--- Game/Collectables/wrench.gd
+++ Game/Collectables/wrench.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+signal wrenchJustCollected
 
 func _ready() -> void:
 	$Appearance.play("idle")
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if(body.name == "LegsPlayer" or body.name == "ArmsPlayer" or body.name == "CompletePlayer"):
+		wrenchJustCollected.emit()
