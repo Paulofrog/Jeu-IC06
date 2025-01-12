70ecb51895c5930b70aad13c32808aee19d212a9
Paul <paul.de.miscault@gmail.com>
2025-01-12 17:50:36 +0100

    final version
    Description contrôles mac
    Exports Windows, Web et macOS

--- Builds/Web/RobotEscape.service.worker.js
+++ Builds/Web/RobotEscape.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1733869194|2955248361';
+const CACHE_VERSION = '1736699147|590105426';
 /** @type {string} */
 const CACHE_PREFIX = 'Jeu-IC06-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
---
+++ Builds/macOS/mac.md
@@ -0,0 +1,3 @@
+# Export macOS
+
+Le fichier pour mac est trop lourd pour passer sur GitHub, il est donc disponible [ici](https://www.swisstransfer.com/d/54f171af-f691-4de0-b293-1d270f8f8e2c) jusqu'au 11/02/2025 à 17h48.
